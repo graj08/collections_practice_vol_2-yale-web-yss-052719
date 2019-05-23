4007b4b46cda8f7f91dfe9d822224822328d41c9
gorgeous-karma-7335 <graj08@gmail.com>
2019-05-23 17:26:16 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -46,14 +46,15 @@ input_array.group_by(&:itself).map do |key,value|
 end
 end
 
-  def merge_data(keys, data)
-    new_keys_1 = {keys[0].keys.to_a[0] => data.to_a[0].to_a[0][0], keys[0].keys.to_a[1] => data.to_a[1].to_a[0][0]}
-    new_data_1 = new_keys_1.merge(data[0].values.to_a[0])
-    new_keys_2 = {keys[1].keys.to_a[1] => data.to_a[0].to_a[1][0]}
-    new_data_2 = new_keys_2.merge(data[0].values.to_a[1])
-   [new_data_1, new_data_2]
- end
+def merge_data(keys, data)
+  new_keys_1 = {keys[0].keys.to_a[0] => data.to_a[0].to_a[0][0]}
+  new_data_1 = new_keys_1.merge(data[0].values.to_a[0])
+  new_keys_2 = {keys[1].keys.to_a[0] => data.to_a[0].to_a[1][0]}
+  new_data_2 = new_keys_2.merge(data[0].values.to_a[1])
+ [new_data_1, new_data_2]
+end
 
+  
 def find_cool (hash)
   if cool[1].values[1] == "cool"
     [cool[1]]
